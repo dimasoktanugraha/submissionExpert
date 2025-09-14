185bcafeb8b60b6f73a75014659088737ef46c8a
dimasoktanugraha <d.oktanugraha@gmail.com>
2025-09-14 11:20:28 +0700

    Initial Commit

---
+++ submissionExpert/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  submissionExpert
+//
+//  Created by Dimas Oktanugraha on 14/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ submissionExpert/submissionExpertApp.swift
@@ -0,0 +1,17 @@
+//
+//  submissionExpertApp.swift
+//  submissionExpert
+//
+//  Created by Dimas Oktanugraha on 14/09/25.
+//
+
+import SwiftUI
+
+@main
+struct submissionExpertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
